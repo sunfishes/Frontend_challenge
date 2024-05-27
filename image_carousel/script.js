1367e4cae4f1b0fb246b53162e1721fe7d7ba7da
SOJEONG KWON <113968044+sunfishes@users.noreply.github.com>
2024-05-27 14:45:45 +0900

    Add files via upload

---
+++ image_carousel/script.js
@@ -0,0 +1,22 @@
+// script.js
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel_image img');
+const totalImages = images.length;
+
+document.querySelector('.slide_next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarousel();
+});
+
+document.querySelector('.slide_prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const carouselImage = document.querySelector('.carousel_image');
+    const width = images[0].clientWidth;
+    carouselImage.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+window.addEventListener('resize', updateCarousel);
